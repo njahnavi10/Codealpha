b9f54865d6ad172d42a801aaf62e64e20102dd91
njahnavi10 <113875116+njahnavi10@users.noreply.github.com>
2023-11-29 11:20:34 +0530

    Add files via upload

---
+++ Gradetracker.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Gradetracker {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of students: ");
+
+        int numStudents = scanner.nextInt();
+
+        // Declare ArrayLists to store student data
+        ArrayList<String> studentNames = new ArrayList<>();
+        ArrayList<Double> studentGrades = new ArrayList<>();
+
+        // Input student data
+        for (int i = 0; i < numStudents; i++) {
+            System.out.print("Enter the name of student " + (i + 1) + ": ");
+            studentNames.add(scanner.next());
+
+            System.out.print("Enter the grade for student " + (i + 1) + ": ");
+            studentGrades.add(scanner.nextDouble());
+        }
+
+        // Compute average, highest, and lowest grades
+        double sum = 0;
+        double highest = Collections.max(studentGrades);
+        double lowest = Collections.min(studentGrades);
+
+        for (double grade : studentGrades) {
+            sum += grade;
+        }
+
+        double average = sum / numStudents;
+
+        // Display results
+        System.out.println("\nHighest Grade: " + highest);
+        System.out.println("Average Grade: " + average);
+        System.out.println("Lowest Grade: " + lowest);
+
+        // Close the scanner
+        scanner.close();
+    }
+}
